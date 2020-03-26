76226cf0a6b9a5f8f53462ce1ebf98cb0bde4c41
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 12:59:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
 
 
 
